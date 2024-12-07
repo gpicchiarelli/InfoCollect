ee22d970508ec595179a865a32630d3cb90df058
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-12-07 12:24:31 +0100

    opml management

--- InfoCollect/opml.pl
+++ InfoCollect/opml.pl
@@ -1,3 +1,59 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use XML::LibXML;
+use Data::Dumper;
+
+# Controllo e installazione dei moduli necessari
+my @modules = qw(XML::LibXML Data::Dumper);
+foreach my $module (@modules) {
+    eval "use $module";
+    if ($@) {
+        print "Il modulo $module non è installato. Installazione in corso...\n";
+        system("cpan -T $module") == 0 or die "Impossibile installare $module\n";
+    }
+}
+
+# Funzione per parsare il file OPML e inserire i dati in una struttura
+sub parse_opml {
+    my ($filename) = @_;
+    my @feeds;
+
+    # Controlla se il file esiste
+    unless (-e $filename) {
+        die "File OPML non trovato: $filename\n";
+    }
+
+    # Crea un nuovo parser XML
+    my $parser = XML::LibXML->new();
+    my $doc = $parser->parse_file($filename);
+
+    # Trova tutti i nodi <outline> con attributo xmlUrl (feed RSS)
+    foreach my $outline ($doc->findnodes('//outline[@xmlUrl]')) {
+        my $title = $outline->getAttribute('title') || 'No Title';
+        my $url   = $outline->getAttribute('xmlUrl');
+
+        # Aggiungi il feed alla struttura dati
+        push @feeds, {
+            title => $title,
+            url   => $url,
+        };
+    }
+
+    return \@feeds;
+}
+
+# Esempio di utilizzo se eseguito direttamente
+if (__FILE__ eq $0) {
+    my $opml_file = 'feeds.opml';  # Cambia con il percorso del tuo file OPML
+
+    my $feeds = parse_opml($opml_file);
+
+    # Stampa la struttura dati risultante
+    print Dumper($feeds);
+}
+
+1;  # Necessario per i moduli Perl
 
 
 
