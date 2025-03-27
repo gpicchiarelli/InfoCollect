0cc1d41625d3424a6c48d3236474d24dba169c6d
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2025-03-27 09:18:26 +0100

    installazione moduli

--- modules_installer.pl
+++ modules_installer.pl
@@ -3,27 +3,67 @@
 use strict;
 use warnings;
 
-my @modules = qw(
-    LWP::UserAgent
-    XML::RSS
-    HTML::TreeBuilder
-    HTML::Strip
-    DBI
-    DBD::SQLite
-    Term::ReadLine
-    Term::ANSIColor
-    Text::Summarizer
-    Lingua::Identify
-    Lingua::EN::Tagger
-    Lingua::IT::Stemmer
-    Text::Extract::Words
-    Parallel::ForkManager
-    Time::HiRes
+# Moduli richiesti per InfoCollect
+
+my @modules = (
+    # --- Database ---
+    'DBI',
+    'DBD::SQLite',
+
+    # --- Crawler e Web ---
+    'LWP::UserAgent',
+    'HTML::TreeBuilder',
+    'HTML::Strip',
+    'XML::RSS',
+
+    # --- NLP e linguistica ---
+    'Text::Summarizer',
+    'Lingua::Identify',
+    'Lingua::IT::Stemmer',
+    'Lingua::EN::Tagger',
+
+    # --- CLI interattiva ---
+    'Term::ReadLine',
+    'Term::ANSIColor',
+
+    # --- Esecuzione parallela ---
+    'Parallel::ForkManager',
+
+    # --- Interfaccia web ---
+    'Mojolicious',
 );
 
-foreach my $mod (@modules) {
-    print "Installazione $mod...\n";
-    system("cpanm $mod");
+# Verifica se cpanm è installato
+sub check_cpanm {
+    print ">> Verifico cpanm...\n";
+    my $cpanm = `which cpanm`;
+    chomp $cpanm;
+    if (!$cpanm) {
+        print ">> cpanm non trovato. Lo installo...\n";
+        system("curl -L https://cpanmin.us | perl - App::cpanminus") == 0
+            or die "Errore durante installazione cpanm: $!";
+    } else {
+        print ">> cpanm trovato: $cpanm\n";
+    }
 }
 
-print "\nInstallazione completata.\n";
+# Installazione moduli
+sub install_modules {
+    foreach my $mod (@modules) {
+        print ">> Controllo modulo $mod...\n";
+        eval "use $mod";
+        if ($@) {
+            print "   >> Non installato. Procedo...\n";
+            system("cpanm $mod") == 0
+                or warn "   !! Errore durante installazione di $mod\n";
+        } else {
+            print "   >> Già installato.\n";
+        }
+    }
+}
+
+# Esecuzione
+check_cpanm();
+install_modules();
+
+print "\n✅ Tutti i moduli sono installati e pronti.\n";
