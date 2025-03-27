b833466b50aca6edf38f6312aa2427ce262198c9
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2025-03-27 09:21:42 +0100

    correzione installazione moduli

--- modules_installer.pl
+++ modules_installer.pl
@@ -3,20 +3,18 @@
 use strict;
 use warnings;
 
-# Moduli richiesti per InfoCollect
-
 my @modules = (
     # --- Database ---
     'DBI',
     'DBD::SQLite',
 
-    # --- Crawler e Web ---
+    # --- Web & parsing ---
     'LWP::UserAgent',
     'HTML::TreeBuilder',
     'HTML::Strip',
     'XML::RSS',
 
-    # --- NLP e linguistica ---
+    # --- NLP & linguistica ---
     'Text::Summarizer',
     'Lingua::Identify',
     'Lingua::IT::Stemmer',
@@ -29,41 +27,49 @@ my @modules = (
     # --- Esecuzione parallela ---
     'Parallel::ForkManager',
 
-    # --- Interfaccia web ---
+    # --- Interfaccia web Mojolicious ---
     'Mojolicious',
 );
 
-# Verifica se cpanm è installato
+my @ok;
+my @failed;
+
 sub check_cpanm {
-    print ">> Verifico cpanm...\n";
-    my $cpanm = `which cpanm`;
-    chomp $cpanm;
-    if (!$cpanm) {
-        print ">> cpanm non trovato. Lo installo...\n";
+    print "Controllo cpanm...\n";
+    my $exists = `which cpanm`;
+    chomp $exists;
+    if (!$exists) {
+        print "Installo cpanm...\n";
         system("curl -L https://cpanmin.us | perl - App::cpanminus") == 0
-            or die "Errore durante installazione cpanm: $!";
-    } else {
-        print ">> cpanm trovato: $cpanm\n";
+            or die "Installazione di cpanm fallita.";
     }
 }
 
-# Installazione moduli
 sub install_modules {
     foreach my $mod (@modules) {
-        print ">> Controllo modulo $mod...\n";
+        print "\n▶ Verifica: $mod\n";
         eval "use $mod";
         if ($@) {
-            print "   >> Non installato. Procedo...\n";
-            system("cpanm $mod") == 0
-                or warn "   !! Errore durante installazione di $mod\n";
+            print "  ↳ Non trovato. Provo installazione...\n";
+            system("cpanm $mod") == 0 ? push(@ok, $mod) : push(@failed, $mod);
         } else {
-            print "   >> Già installato.\n";
+            print "  ↳ Già installato.\n";
+            push @ok, $mod;
         }
     }
 }
 
-# Esecuzione
 check_cpanm();
 install_modules();
 
-print "\n✅ Tutti i moduli sono installati e pronti.\n";
+# Riepilogo
+print "\n📦 Moduli installati correttamente:\n";
+print " - $_\n" for @ok;
+
+if (@failed) {
+    print "\n❌ Moduli che NON sono stati installati correttamente:\n";
+    print " - $_\n" for @failed;
+    print "\n⚠️ Controlla la connessione o i permessi di installazione.\n";
+} else {
+    print "\n✅ Tutti i moduli richiesti sono stati installati o erano già presenti.\n";
+}
