3690d0af8beccec63e51be2c47a35b09cd11cfe3
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2025-03-27 10:19:48 +0100

    modified:   lib/db.pm

--- lib/db.pm
+++ lib/db.pm
@@ -39,3 +39,182 @@ sub add_rss_feed {
     $sth->finish();
     $dbh->disconnect();
 }
+
+# Funzione per ottenere tutti i feed RSS
+sub get_all_rss_feeds {
+    my $dbh = connect_db();
+    my $sth = $dbh->prepare("SELECT id, title, url FROM rss_feeds");
+    $sth->execute();
+
+    my @feeds;
+    while (my $row = $sth->fetchrow_hashref) {
+        push @feeds, $row;
+    }
+
+    $sth->finish();
+    $dbh->disconnect();
+    return \@feeds;
+}
+
+# Funzione per aggiungere un URL web
+sub add_web_url {
+    my ($url) = @_;
+
+    unless ($url) {
+        die "Errore: URL è richiesto per aggiungere un URL web.\n";
+    }
+
+    my $dbh = connect_db();
+    my $sth = $dbh->prepare("INSERT INTO web (url, attivo) VALUES (?, 1)");
+    eval {
+        $sth->execute($url);
+    };
+    if ($@) {
+        warn "Errore durante l'inserimento dell'URL web: $@";
+    } else {
+        print "URL web aggiunto con successo: $url\n";
+    }
+    $sth->finish();
+    $dbh->disconnect();
+}
+
+# Funzione per ottenere tutti gli URL web
+sub get_all_web_urls {
+    my $dbh = connect_db();
+    my $sth = $dbh->prepare("SELECT id, url, attivo FROM web");
+    $sth->execute();
+
+    my @urls;
+    while (my $row = $sth->fetchrow_hashref) {
+        push @urls, $row;
+    }
+
+    $sth->finish();
+    $dbh->disconnect();
+    return \@urls;
+}
+
+# Funzione per aggiornare lo stato di un URL web (attivo/inattivo)
+sub update_web_url_status {
+    my ($id, $status) = @_;
+
+    unless (defined $id && defined $status) {
+        die "Errore: ID e stato sono richiesti per aggiornare lo stato di un URL web.\n";
+    }
+
+    my $dbh = connect_db();
+    my $sth = $dbh->prepare("UPDATE web SET attivo = ? WHERE id = ?");
+    eval {
+        $sth->execute($status, $id);
+    };
+    if ($@) {
+        warn "Errore durante l'aggiornamento dello stato dell'URL web: $@";
+    } else {
+        print "Stato dell'URL web aggiornato con successo: ID=$id, Stato=$status\n";
+    }
+    $sth->finish();
+    $dbh->disconnect();
+}
+
+# Funzione per aggiungere una nuova impostazione
+sub add_setting {
+    my ($key, $value) = @_;
+
+    unless ($key && defined $value) {
+        die "Errore: chiave e valore sono richiesti per aggiungere una impostazione.\n";
+    }
+
+    my $dbh = connect_db();
+    my $sth = $dbh->prepare("INSERT INTO settings (key, value) VALUES (?, ?) ON CONFLICT(key) DO UPDATE SET value = excluded.value");
+    eval {
+        $sth->execute($key, $value);
+    };
+    if ($@) {
+        warn "Errore durante l'aggiunta o l'aggiornamento dell'impostazione: $@";
+    } else {
+        print "Impostazione aggiunta o aggiornata: $key = $value\n";
+    }
+    $sth->finish();
+    $dbh->disconnect();
+}
+
+# Funzione per ottenere tutte le impostazioni
+sub get_all_settings {
+    my $dbh = connect_db();
+    my $sth = $dbh->prepare("SELECT key, value FROM settings");
+    $sth->execute();
+
+    my %settings;
+    while (my $row = $sth->fetchrow_hashref) {
+        $settings{$row->{key}} = $row->{value};
+    }
+
+    $sth->finish();
+    $dbh->disconnect();
+    return \%settings;
+}
+
+# Funzione per eliminare un'impostazione
+sub delete_setting {
+    my ($key) = @_;
+
+    unless ($key) {
+        die "Errore: chiave è richiesta per eliminare un'impostazione.\n";
+    }
+
+    my $dbh = connect_db();
+    my $sth = $dbh->prepare("DELETE FROM settings WHERE key = ?");
+    eval {
+        $sth->execute($key);
+    };
+    if ($@) {
+        warn "Errore durante l'eliminazione dell'impostazione: $@";
+    } else {
+        print "Impostazione eliminata: $key\n";
+    }
+    $sth->finish();
+    $dbh->disconnect();
+}
+
+# Funzione per verificare se un'impostazione esiste
+sub setting_exists {
+    my ($key) = @_;
+
+    unless ($key) {
+        die "Errore: chiave è richiesta per verificare l'esistenza di un'impostazione.\n";
+    }
+
+    my $dbh = connect_db();
+    my $sth = $dbh->prepare("SELECT 1 FROM settings WHERE key = ?");
+    $sth->execute($key);
+
+    my $exists = $sth->fetchrow_arrayref ? 1 : 0;
+
+    $sth->finish();
+    $dbh->disconnect();
+    return $exists;
+}
+
+1;
+
+# Licenza BSD
+# -----------------------------------------------------------------------------
+# Copyright (c) 2024, Giacomo Picchiarelli
+# All rights reserved.
+#
+# Ridistribuzione e uso nel formato sorgente e binario, con o senza modifiche,
+# sono consentiti purché siano soddisfatte le seguenti condizioni:
+#
+# 1. Le ridistribuzioni del codice sorgente devono conservare l'avviso di copyright
+#    di cui sopra, questo elenco di condizioni e il seguente disclaimer.
+# 2. Le ridistribuzioni in formato binario devono riprodurre l'avviso di copyright,
+#    questo elenco di condizioni e il seguente disclaimer nella documentazione
+#    e/o nei materiali forniti con la distribuzione.
+# 3. Né il nome dell'autore né i nomi dei suoi collaboratori possono essere utilizzati
+#    per promuovere prodotti derivati da questo software senza un'autorizzazione
+#    specifica scritta.
+#
+# QUESTO SOFTWARE È FORNITO "COSÌ COM'È" E QUALSIASI GARANZIA ESPRESSA O IMPLICITA
+# È ESCLUSA. IN NESSUN CASO L'AUTORE SARÀ RESPONSABILE PER DANNI DERIVANTI
+# DALL'USO DEL SOFTWARE.
+# -----------------------------------------------------------------------------
